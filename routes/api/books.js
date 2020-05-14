33221148239bebe505ebc7cc6e7032cfed82ae82
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-05-13 19:31:19 -0700

    routes and things

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,5 +1,5 @@
-const router = require("express").Router()
-const bookController = require("../../controller/bookController")
-router.route("/").get(bookController.findAll).post(bookController.create)
-router.route("/:id").get(bookController.findById).put(bookController.upDate).delete(bookController.delete)
-module.exports = router
+const router = require("express").Router();
+const bookController = require("../../controllers/bookController");
+router.route("/").get(bookController.findAll).post(bookController.create);
+router.route("/:id").get(bookController.findById).put(bookController.upDate).delete(bookController.remove);
+module.exports = router;
