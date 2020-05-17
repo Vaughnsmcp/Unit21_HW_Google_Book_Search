080b5d4ced6f109f195be0b5bd661602abb2586e
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-05-16 20:37:25 -0700

    JUMBOTRONgit add .

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,18 @@
 //This is where we add components handleInputChange, handleFormSubmit, get books, handle save books
-import React from "react"
-const Home=()=>(
+import React from "react";
+import Header from "../components/Header";
 
-    <div>
-        <h1>This is Vaughn!</h1>
-    </div>
-)
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+        )
+       
+}
+
+
+
 
 
 export default Home
