06e8eeb511d5579a7f62cb70899f87a4af7aebfc
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-05-17 22:15:35 -0700

    quick back step

--- server.js
+++ server.js
@@ -2,18 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const path = require("path");
 const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api/items", items);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  app.get("*", (reg, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-
-  });
 }
 app.use(routes);
 mongoose.connect(
